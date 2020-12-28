38c1a4f3127138d66d242c4da9756902eadb9526
Leigh Stewart <leigh@macbook-pro.lan>
2020-12-28 14:45:17 -0800

    reverrt trigger

--- packages/slate-plugins/src/common/queries/isWordAfterTrigger.ts
+++ packages/slate-plugins/src/common/queries/isWordAfterTrigger.ts
@@ -1,6 +1,6 @@
-import { Editor, Point } from "slate";
-import { escapeRegExp } from "../utils";
-import { getText } from "./getText";
+import { Editor, Point } from 'slate';
+import { escapeRegExp } from '../utils';
+import { getText } from './getText';
 
 /**
  * Is the word at the point after a trigger (punctuation character)
@@ -11,29 +11,11 @@ export const isWordAfterTrigger = (
   { at, trigger }: { at: Point; trigger: string }
 ) => {
   // Point at the start of previous word (excluding punctuation)
-  const wordBefore = Editor.before(editor, at, { unit: "word" });
-  /*
+  const wordBefore = Editor.before(editor, at, { unit: 'word' });
+
   // Point before wordBefore
-  const before =
-    wordBefore &&
-    Editor.before(editor, wordBefore, { unit: "character", distance: 2 });
-  */
+  const before = wordBefore && Editor.before(editor, wordBefore);
 
-  const lineStart = Editor.before(editor, at, { unit: "line" });
-  const lineRange = lineStart && Editor.range(editor, lineStart, at);
-  const lineText = getText(editor, lineRange);
-  const lastOpenMatch = lineText.match(/^.*\[\[(.+)/);
-  const matchFromLineStart = lastOpenMatch && lastOpenMatch[0];
-  const matchFromMentionStart = lastOpenMatch && lastOpenMatch[1];
-  const prefixLen = matchFromLineStart?.length - matchFromMentionStart?.length;
-  const beforeRangeStart = Editor.after(editor, lineStart, {
-    unit: "character",
-    distance: prefixLen,
-  });
-  const beforeRange2 = Editor.range(editor, beforeRangeStart, at);
-
-  //console.dir(beforeRange2);
-  /*
   // Range from before to start
   const beforeRange = before && Editor.range(editor, before, at);
 
@@ -45,18 +27,10 @@ export const isWordAfterTrigger = (
   const beforeRegex = new RegExp(`^${escapedTrigger}(\\w+)$`);
 
   // Match regex on before text
-  const match = !!beforeText && beforeText.match(/^\[\[(\w+)$/);
-
-  console.dir(beforeText);
-  console.dir(match);
+  const match = !!beforeText && beforeText.match(beforeRegex);
 
   return {
     range: beforeRange,
     match,
-  };
-  */
-  return {
-    range: beforeRange2,
-    match: lastOpenMatch,
   };
 };
